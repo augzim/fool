6924a33e36656e0487e1f78187ccd08ef7e22545
AugZim <augustzim93@gmail.com>
2023-09-12 19:57:21 +0400

    Change the access way of settings.cfg (config.py)

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 """Provide a configuration settings for an entire game with a CONFIG dict."""
 
 
+from pathlib import Path
+
+
 # TODO: make it using configparser module
-with open('settings.cfg') as config:
+
+settings = Path(__file__).parent / 'settings.cfg'
+
+with open(settings) as config:
     CONFIG = {}
 
     for line in config:
